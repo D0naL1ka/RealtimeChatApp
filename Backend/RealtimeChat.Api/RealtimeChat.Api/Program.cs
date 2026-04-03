5b68e7899a2f177392328fb629b1ea36f13b67be
D0naL1ka <alina.suchok00@gmail.com>
2026-04-03 20:51:46 +0300

    Fix: complete database configuration in Program and appsettings

--- Backend/RealtimeChat.Api/RealtimeChat.Api/Program.cs
+++ Backend/RealtimeChat.Api/RealtimeChat.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RealtimeChatApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlDbConnection")));
 
 var app = builder.Build();
 
