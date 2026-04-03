ce8420d4a419ccc73b6ff53ef758ae2b7d32af12
D0naL1ka <alina.suchok00@gmail.com>
2026-04-03 20:09:39 +0300

    Аdd ChatMessage model and AppDbContext

---
+++ Backend/RealtimeChatApp.Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealtimeChatApp.Data.Models;
+using System.Reflection.Emit;
+
+
+namespace RealtimeChatApp.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<ChatMessage> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChatMessage>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username)
+                      .HasMaxLength(100)
+                      .IsRequired();
+                entity.Property(e => e.Text)
+                      .HasMaxLength(2000)
+                      .IsRequired();
+                entity.Property(e => e.Sentiment)
+                      .HasMaxLength(20);
+            });
+        }
+    }
+}
